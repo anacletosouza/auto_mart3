7486891aaae63fa9aecbd6babefd18a71110c2c3
Anacleto Silva de Souza <anacletosilvadesouza@usp.br>
2026-08-18 04:11:37 +0200

    update

--- build/lib/auto_mart3/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Auto_Mart3 - Tools for coarse-grained molecular dynamics."""
-
-__version__ = "1.0.0"
-__author__ = "Anacleto Silva de Souza"
-
-from . import scripts
-from .cli import main
-
-__all__ = ["main", "scripts"]
